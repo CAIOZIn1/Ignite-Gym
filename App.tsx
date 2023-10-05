24b55201b1cb31cfd4044d806e3d0724be280740
Caio Sella <sellacaio4@gmail.com>
2023-10-04 23:27:41 -0300

    First commit - added some styles and fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
+import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
 
-export default function App() {
+export default function app(){
+  const [fontsLoaded] = useFonts({Nunito_400Regular, Nunito_700Bold});
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202824'}}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
+      {fontsLoaded ? <Text style={{ fontFamily: 'Nunito_700Bold', color: '#fff' }}>Olá Mundo</Text> : <View />}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
