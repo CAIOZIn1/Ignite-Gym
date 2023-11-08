dd2c00ff9d6a11c00f99bcb46dc012bd092ce0a4
Caio Sella <sellacaio4@gmail.com>
2023-11-08 15:00:16 -0300

    implementing database and registering the user

--- mobile/src/screens/SingnUp.tsx
+++ mobile/src/screens/SingnUp.tsx
@@ -1,13 +1,13 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
-import { useForm, Controller } from "react-hook-form";
-import {yupResolver} from '@hookform/resolvers/yup'
+import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
+import { Controller, useForm } from "react-hook-form";
 import * as yup from 'yup';
 
-import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
-import Input from "@components/Input";
+import LogoSvg from "@assets/logo.svg";
 import Button from "@components/Button";
+import Input from "@components/Input";
 
 type FormDataProps = {
   name: string;
@@ -38,13 +38,22 @@ const SignUp = () => {
     navigation.goBack();
   }
 
-  function handleSignUp({
+  async function handleSignUp({
     name,
     email,
     password,
     password_confirm,
   }: FormDataProps) {
+    const response = await fetch('http://192.168.15.9:3333/users', {
+      method: 'POST',
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name, email, password})
+    })
 
+    const data = await response.json();
   }
 
   return (
